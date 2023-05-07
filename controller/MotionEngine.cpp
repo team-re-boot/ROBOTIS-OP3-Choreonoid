a0c4eda3aa561ce9e44ebf41d8484c274f23f8bf
RyuYamamoto <ryu.yamamoto@tier4.jp>
2023-05-07 23:04:59 +0900

    fix: remove kinematics

--- controller/MotionEngine.cpp
+++ controller/MotionEngine.cpp
@@ -20,49 +20,13 @@ void MotionEngine::initialize(cnoid::SimpleControllerIO * io)
     joint_angle.prev_q = joint_angle.ref_q;
     joint_angles_[joint_angle.name] = joint_angle;
   }
-
-  kinematics_ = std::make_shared<Kinematics>(body_);
-  kinematics_->calcForwaredKinematicsAll();
-  kinematics_->setRefAngle(joint_angles_);
-  kinematics_->getFootPos(right_foot_pos_, right_foot_rot_, left_foot_pos_, left_foot_rot_);
-}
-
-// motion command generate
-void MotionEngine::receiveCommand(double * l_stick, double * r_stick)
-{
-  joystick_.readCurrentState();
-
-  // get joy stick value[-1. ~ 1.]
-  l_stick[0] = joystick_.getPosition(cnoid::Joystick::L_STICK_H_AXIS);
-  l_stick[1] = joystick_.getPosition(cnoid::Joystick::L_STICK_V_AXIS);
-  r_stick[0] = joystick_.getPosition(cnoid::Joystick::R_STICK_H_AXIS);
-  r_stick[1] = joystick_.getPosition(cnoid::Joystick::R_STICK_V_AXIS);
 }
 
 // main controller
-void MotionEngine::control()
-{
-  // get command from joy stick
-  double l_stick[2];
-  double r_stick[2];
-  receiveCommand(l_stick, r_stick);
-
-  // TODO: ik demo
-  left_foot_pos_.z() += (0.00005 * l_stick[0]);
-  right_foot_pos_.z() += (0.00005 * l_stick[0]);
-
-  //  kinematics_->setRefAngle(joint_angles_);
-  bool result = false;
-  if (
-    kinematics_->calcInverseKinematics(right_foot_pos_, right_foot_rot_, "r_ank_roll") and
-    kinematics_->calcInverseKinematics(left_foot_pos_, left_foot_rot_, "l_ank_roll"))
-    joint_angles_ = kinematics_->getRefAngle();
-
-  jointControl();
-}
+void MotionEngine::control() { joint_control(); }
 
 // torque controller by pd control
-void MotionEngine::jointControl()
+void MotionEngine::joint_control()
 {
   for (std::size_t idx = 0; idx < body_->numJoints(); ++idx) {
     cnoid::Link * joint = body_->joint(idx);
